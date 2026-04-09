c39905fce27f4fac10b8480bf674fe07052a7d3a
Janvi Sharma <sjjanvi2121@gmail.com>
2026-04-09 22:02:49 +0530

    Fix email transporter secure flag for Gmail ports

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -5,6 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
